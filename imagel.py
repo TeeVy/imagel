cee5d542db9d6ca699921d0d78d2fb76228a3370
TeeVy <carlorobin@gmail.com>
2017-06-02 11:40:09 +0200

    Clean v2.7.0-alpha, here is the v2.8.0-alpha!
    In fact, it's not just a cleaning, I rewrote and create some functions, more cleanliness, less mess.

--- imagel.py
+++ imagel.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from tkinter.ttk import *
 import tkinter.filedialog
+from tkinter import messagebox
 import cv2
 import glob
 import os
@@ -8,50 +9,12 @@ import webbrowser
 import configparser
 import clipboard
 
-#Vérification de config.ini
-config = configparser.ConfigParser()
-
-try:
-  with open('config.ini'):
-    print('Le fichier "config.ini" a été chargé avec succès !')
-except IOError:
-  print('Le fichier "config.ini" est introuvable, Imagel va automatiquement en créer un...')
-  config['default'] = {'destination': ''}
-  config['custom'] = {'destination': ''}
-  with open('config.ini', 'w') as configfile:
-    config.write(configfile)
-  print('Le fichier "config.ini" a été créé avec succès !')
-
-config.read('config.ini')
-destination = config['custom']['destination']
-
-if destination == '':
-  destination = os.getcwd() + '/'
-  destination = str.replace(destination, '\\', '/')
-  config.set('default','destination',destination)
-  config.set('custom','destination',destination)
-  with open('config.ini', 'w') as configfile:
-    config.write(configfile)
-
-destination_check = os.path.isdir(destination)
-
-if destination_check == False:
-  print('\nErreur ! Le dossier de sortie est introuvable !')
-  print('La destination a été remise par défaut')
-  destination = config['default']['destination']
-  config.set('custom','destination',destination)
-  with open('config.ini', 'w') as configfile:
-    config.write(configfile)
-
-#Vérifications terminées, on peut désormais lancer le script.
-print('\nBienvenue dans Imagel, la destination de sortie se trouve actuellement dans :\n' + destination)
-
 def Destination():
   global destination
   new_destination = tkinter.filedialog.askdirectory(title='Changer la destination de sortie', initialdir=destination)
   if new_destination != '':
     destination = new_destination + '/'
-    config.set('custom','destination',destination)
+    config.set('default','destination',destination)
     with open('config.ini', 'w') as configfile:
       config.write(configfile)
     print('\nDestination de sortie modifiée : \n' + destination)
@@ -61,6 +24,46 @@ def Destination():
     T_destination.config(state=DISABLED)
   else:
     print('\nChangement de la destination de sortie annulée')
+
+#Vérification de config.ini
+config = configparser.ConfigParser()
+
+def CreateIni():
+  global destination
+  destination = os.getcwd() + '/'
+  destination = str.replace(destination, '\\', '/')
+  config['default'] = {'destination': destination}
+  with open('config.ini', 'w') as configfile:
+    config.write(configfile)
+
+try:
+  with open('config.ini'):
+    print('Le fichier "config.ini" a été chargé avec succès !')
+except IOError:
+  print('Le fichier "config.ini" est introuvable, Imagel va automatiquement en créer un...')
+  CreateIni()
+  print('Le fichier "config.ini" a été créé avec succès !')
+
+config.read('config.ini')
+destination = config['default']['destination']
+
+#Vérification de l'existence du dossier de sortie
+def DestinationCheck(destination):
+  global destination_check
+  destination_check = os.path.isdir(destination)
+  if destination_check == False:
+    print('\nErreur ! Le dossier de sortie est introuvable !')
+    if 'window' in globals():
+      messagebox.showerror('Imagel - Erreur', 'Le dossier de sortie est introuvable !\nVeuillez changer la destination de ce dernier')
+      Destination()
+    else:
+      CreateIni()
+      print('La destination du dossier de sortie a été rénitialisée')
+
+DestinationCheck(destination)
+
+#Vérifications terminées, on peut désormais lancer le script
+print('\nBienvenue dans Imagel, la destination de sortie se trouve actuellement dans :\n' + destination)
 
 def PopCopy(event):
   T_destination.focus_force()
@@ -72,60 +75,64 @@ def Copy(destination):
   print('\nChemin du dossier de sortie copié dans le presse-papier\n' + destination)
 
 def ParDossier():
-  directory = tkinter.filedialog.askdirectory(title='Choisir un dossier à traiter', initialdir='.')
-  if directory != '':
-    print('\nDossier sélectionné :', directory)
-    files = glob.glob(directory + '/*.mov') + glob.glob(directory + '/*.mp4')
-    for filename in files:
-      filename = filename.split('\\')
-      del filename[0]
-      filename = str.join('', (filename))
-      print('\nOpération en cours sur :', filename)
-      cap = cv2.VideoCapture(directory + '/' + filename)
-      length = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-      print('Nombre de frames de la vidéo :', length)
-      cap.set(1,length);
-      ret, frame = cap.read()
-      print ('Capture de l\'image', length, ':', ret)
-      while ret==False:
-        print('Échec de la capture, recul d\'une image...')
-        length = length-1
+  DestinationCheck(destination)
+  if destination_check == True:
+    directory = tkinter.filedialog.askdirectory(title='Choisir un dossier à traiter', initialdir='.')
+    if directory != '':
+      print('\nDossier sélectionné :', directory)
+      files = glob.glob(directory + '/*.mov') + glob.glob(directory + '/*.mp4')
+      for filename in files:
+        filename = filename.split('\\')
+        del filename[0]
+        filename = str.join('', (filename))
+        print('\nOpération en cours sur :', filename)
+        cap = cv2.VideoCapture(directory + '/' + filename)
+        length = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+        print('Nombre de frames de la vidéo :', length)
         cap.set(1,length);
         ret, frame = cap.read()
         print ('Capture de l\'image', length, ':', ret)
-      gel = filename[0:-4]
-      cv2.imwrite(destination + gel + '.jpg', frame)
-    print('\nOpérations terminées !')
-    Copy(destination)
-  else:
-    print('\nAucun dossier sélectionné')
+        while ret==False:
+          print('Échec de la capture, recul d\'une image...')
+          length = length-1
+          cap.set(1,length);
+          ret, frame = cap.read()
+          print ('Capture de l\'image', length, ':', ret)
+        gel = filename[0:-4]
+        cv2.imwrite(destination + gel + '.jpg', frame)
+      print('\nOpérations terminées !')
+      Copy(destination)
+    else:
+      print('\nAucun dossier sélectionné')
 
 def ParFichiers():
-  files = tkinter.filedialog.askopenfilenames(title='Choisir un ou des fichiers à traiter', filetypes=[('Vidéos','*.mov;*.mp4')], initialdir='.')
-  if files !='':
-    for filename in files:
-      file = str.join('', (filename))
-      filename = os.path.basename(file)
-      print('\nOpération en cours sur :', filename)
-      cap = cv2.VideoCapture(file)
-      length = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-      print('Nombre de frames de la vidéo :', length)
-      cap.set(1,length);
-      ret, frame = cap.read()
-      print ('Capture de l\'image', length, ':', ret)
-      while ret==False:
-        print('Échec de la capture, recul d\'une image...')
-        length = length-1
+  DestinationCheck(destination)
+  if destination_check == True:
+    files = tkinter.filedialog.askopenfilenames(title='Choisir un ou des fichiers à traiter', filetypes=[('Vidéos','*.mov;*.mp4')], initialdir='.')
+    if files !='':
+      for filename in files:
+        file = str.join('', (filename))
+        filename = os.path.basename(file)
+        print('\nOpération en cours sur :', filename)
+        cap = cv2.VideoCapture(file)
+        length = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+        print('Nombre de frames de la vidéo :', length)
         cap.set(1,length);
         ret, frame = cap.read()
         print ('Capture de l\'image', length, ':', ret)
-      gel = filename[0:-4]
-      print(destination + gel)
-      cv2.imwrite(destination + gel + '.jpg', frame)
-    print('\nOpération(s) terminée(s) !')
-    Copy(destination)
-  else:
-    print('\nAucun fichier sélectionné')
+        while ret==False:
+          print('Échec de la capture, recul d\'une image...')
+          length = length-1
+          cap.set(1,length);
+          ret, frame = cap.read()
+          print ('Capture de l\'image', length, ':', ret)
+        gel = filename[0:-4]
+        print(destination + gel)
+        cv2.imwrite(destination + gel + '.jpg', frame)
+      print('\nOpération(s) terminée(s) !')
+      Copy(destination)
+    else:
+      print('\nAucun fichier sélectionné')
 
 def Github():
   webbrowser.open("https://github.com/TeeVy/imagel")
@@ -144,12 +151,12 @@ MenuFichier.add_command(label='Quitter',command=window.destroy)
 M.add_cascade(label='Fichier', menu=MenuFichier)
 
 MenuApropos = Menu(M,tearoff=0, activebackground='#91c9f7', activeforeground='black')
-MenuApropos.add_command(label="Github",command= Github)
-M.add_cascade(label="À propos", menu=MenuApropos)
+MenuApropos.add_command(label='Github',command= Github)
+M.add_cascade(label='À propos', menu=MenuApropos)
 
 window.config(menu=M)
 
-logo = PhotoImage(file="img/imagel_logo.png")
+logo = PhotoImage(file='img/imagel_logo.png')
 
 canvas = Canvas(window,width=317, height=98)
 canvas.create_image(0, 0, anchor=NW, image=logo)
@@ -172,7 +179,7 @@ B_destination.pack(pady=5, padx=10, side = RIGHT)
 
 #Copy
 M_copy = Menu(T_destination, tearoff=0)
-M_copy.add_command(label="Copier", accelerator="Ctrl+C", command = lambda: Copy(destination))
+M_copy.add_command(label='Copier', command = lambda: Copy(destination))
 T_destination.bind("<Button-3>", PopCopy)
 
 #Par dossier
